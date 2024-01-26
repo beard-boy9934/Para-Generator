56c988db00380555b14dcd3fc6e7126b58a4b723
Sunny Kumar <kmr.sny913520@gmail.com>
2024-01-26 10:57:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const input=document.getElementById("numOfwords");
+const container=document.querySelector(".container");
+
+const generateWord=(n)=>{
+    let text="";
+    const letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(let i=0;i<n;i++)
+    {
+        const random=(Math.random()*25).toFixed(0);
+        text+=letters[random];
+    }
+    return text;
+};
+
+const generatePara=()=>{
+     const numOfwords=Number(input.value);
+     const para=document.createElement("p");
+     let data="";
+    for(let i=0;i<numOfwords;i++)
+    {
+        const randomNumber=(Math.random()*15).toFixed(0);
+        data+=generateWord(randomNumber);
+        data+=" ";
+    }
+    para.innerText=data;
+    para.setAttribute("class","paras");
+    container.append(para);
+}
